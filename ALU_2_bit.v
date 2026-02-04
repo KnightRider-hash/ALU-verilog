e677b93bf0539ec8c697fe8bd3feb3ac6566ec65
Gopanandhan PD <gopanandhanpd05@gmail.com>
2026-02-04 22:24:11 +0530

    Update ALU_n_bit.v

--- ALU_2_bit.v
+++ ALU_2_bit.v
@@ -5,7 +5,7 @@
 // 
 // Create Date: 28.01.2026 22:30:39
 // Design Name: 
-// Module Name: ALU_2_bit
+// Module Name: ALU_n_bit
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -20,33 +20,32 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module ALU_2_bit(
-   input [1:0] A,
-input [1:0] B,
+module ALU_n_bit
+#(parameter n=31)
+(input [n:0] A,
+input [n:0] B,
 input [2:0] OP,
 output reg z,c,
-output reg [1:0] out
-    );
-    always@(*)begin
+output reg [n:0] out
+);
+always@(*)begin
 c=1'b0;
 case(OP)
-3'b000: begin
-        out= A+B;
-        c = (A[1]&B[1]) | (A[0]&B[0]&(A[1] | B[1]));
-        end
+3'b000:{c,out}=A+B;
+        
 3'b001: begin
         out= A-B;
-        c=(A<B)?1'b1:1'b0;
+        c=(A<B);
         end
 3'b010: out= A*B;
-3'b011: out= (A==0||B==0)?2'b00:A/B;
+3'b011: out= (A==0||B==0)?0:A/B;
 3'b100: out= A&B;
 3'b101: out= A|B;
 3'b110: out= A^B;
 3'b111: out= ~A;
-default:out=2'bxx;
+default:out=0;
 endcase 
 z=1'b0;
-z=(out==2'b00)?1'b1:1'b0;
+z=(out==0)?1'b1:1'b0;
 end
 endmodule
